233467beb4b44fab6226640a365aba483067f3a9
AllieSaxton03 <allie.saxton@gmail.com>
2023-10-03 16:46:30 -0500

    created useful sql files for automatically clearing and loading data

---
+++ copy_data_from_csv.sql
@@ -0,0 +1,6 @@
+\copy employee from 'employee.csv' CSV HEADER;
+\copy customer from  'customer.csv' CSV HEADER;
+\copy orders from 'orders.csv' CSV HEADER;
+\copy drink from 'drink.csv' CSV HEADER;
+\copy topping from 'topping.csv' CSV HEADER;
+\copy drink_topping from 'drink_topping.csv' CSV HEADER;
